7d7bf7d8c17bde08068524a2a9c54aad79eeb6de
youjie <youjie@weiche.cn>
2018-11-13 16:49:38 +0800

    u

--- spark/src/main/java/cn/weiche/www/App.java
+++ spark/src/main/java/cn/weiche/www/App.java
@@ -1,7 +1,14 @@
 package cn.weiche.www;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("hello");
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
+
 }
---
+++ spark/src/main/java/cn/weiche/www/JDBCSSHChannel.java
@@ -0,0 +1,67 @@
+package cn.weiche.www;
+
+import com.jcraft.jsch.*;
+
+import java.sql.*;
+
+public class JDBCSSHChannel {
+//    public static void main(String[] args) {
+//            goSSH(3306,"martin-order-slave-offline.mysql.service.consul",3306,"youjie","sa.shequan.com",22);
+//            connDB();
+//        //} catch (Exception e) {
+//         //   System.err.println(e);
+//      //  }
+//    }
+    /**
+     *
+     * @param localPort  本地host 建议mysql 3306
+     * @param rhost    host 远程MySQL服务器
+     * @param rport    port 3306
+     * @param user   ssh 用户名
+     * @param host   ssh主机
+     * @param port   远程机器端口22
+     */
+    public static void goSSH(int localPort, String rhost, int rport,
+                             String user, String host,
+                             int port) {
+        try {
+            JSch jsch = new JSch();
+            jsch.addIdentity("C:\\Users\\Administrator\\.ssh\\id_rsa");
+            Session session = jsch.getSession(user, host, port);
+            //session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            System.out.println(session.getServerVersion());//这里打印SSH服务器版本信息
+            int assinged_port = session.setPortForwardingL(localPort, rhost, rport);
+            System.out.println("localhost:" + assinged_port + " -> " + rhost + ":" + rport);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void connDB() {
+        Connection conn = null;
+        ResultSet rs = null;
+        Statement st = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/martin_order", "bigdata_dev", "fDNdCFAOfP");
+            st = conn.createStatement();
+            String sql = "SELECT * FROM order_base limit 1";
+            rs = st.executeQuery(sql);
+            while (rs.next())
+                System.out.println(rs.getString(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                st.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+}
